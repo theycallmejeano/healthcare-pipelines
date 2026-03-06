ba68bf6ff39c72e7a4f8e3dea9ba65999ad4d7eb
theycallmejeano <jeangithae@outlook.com>
2026-03-06 20:35:02 +0300

    comments and variable renaming

--- src/health_pipeline/assets/dhis2_assets.py
+++ src/health_pipeline/assets/dhis2_assets.py
@@ -1,3 +1,7 @@
+"""
+Assets to pull data from the demo API end points
+"""
+
 import dagster as dg
 import io
 import pandas as pd
@@ -9,6 +13,7 @@ ANALYTICS_ENDPOINT = "/api/analytics.csv"
 
 
 class AnalyticsConfig(dg.Config):
+    # TODO yaml or something to store these?
     data_elements: str = "Dkapzovo8Ll;BXgDHhPdFVU;dY4OCwl0Y7Y"
     period: str = "LAST_12_MONTHS"
     org_unit_level: str = "LEVEL-4"
@@ -16,9 +21,10 @@ class AnalyticsConfig(dg.Config):
 
 @dg.asset(description="Fetches all organisation units from the DHIS2 API")
 def raw_org_units(context: dg.AssetExecutionContext, dhis2: DHIS2Resource) -> dict:
-    data = dhis2.get_dhis2_json(ORG_UNITS_ENDPOINT, params={"paging": False})
-    context.log.info(f"Fetched {len(data['organisationUnits'])} org units")
-    return data
+    # load org units as json file
+    orgunits = dhis2.get_dhis2_json(ORG_UNITS_ENDPOINT, params={"paging": False})
+    context.log.info(f"Fetched {len(orgunits['organisationUnits'])} org units")
+    return orgunits
 
 
 @dg.asset(description="Fetches analytics data from the DHIS2 API.")
@@ -27,6 +33,7 @@ def raw_analytics(
     config: AnalyticsConfig,
     dhis2: DHIS2Resource,
 ) -> pd.DataFrame:
+    # fetch dataelement csv
     csv_text = dhis2.get_dhis2_csv(
         ANALYTICS_ENDPOINT,
         params={
